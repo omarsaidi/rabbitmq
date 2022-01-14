f43995d94788aae856aa1485dfac591fa5bc612f
Omar SAIDI <omar.saidi@mediawan.com>
2022-01-14 10:32:38 +0100

    work queue

--- work-queues/NewTask/NewTask.cs
+++ work-queues/NewTask/NewTask.cs
@@ -6,15 +6,16 @@ class NewTask
 {
     public static void Main(string[] args)
     {
-        var factory = new ConnectionFactory { HostName = "localhost" };
+        var factory = new ConnectionFactory() { HostName = "localhost" };
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: "task_queue",
-                                 durable: false,
+                                 durable: true,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
+
             var message = GetMessage(args);
             var body = Encoding.UTF8.GetBytes(message);
 
@@ -25,7 +26,9 @@ class NewTask
                                  routingKey: "task_queue",
                                  basicProperties: properties,
                                  body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
         }
+
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
     }
--- work-queues/Worker/Worker.cs
+++ work-queues/Worker/Worker.cs
@@ -6,17 +6,22 @@ using System.Threading;
 
 class Worker
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         var factory = new ConnectionFactory() { HostName = "localhost" };
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: "task_queue",
-                                 durable: false,
+                                 durable: true,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (sender, ea) =>
             {
@@ -29,11 +34,14 @@ class Worker
 
                 Console.WriteLine(" [x] Done");
 
-                // Note: it is possible to access the channel via
-                //       ((EventingBasicConsumer)sender).Model here
-                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    // Note: it is possible to access the channel via
+                    //       ((EventingBasicConsumer)sender).Model here
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             };
-            channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+            channel.BasicConsume(queue: "task_queue",
+                                 autoAck: false,
+                                 consumer: consumer);
+
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
         }
