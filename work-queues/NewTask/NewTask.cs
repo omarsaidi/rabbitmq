c8c52c831a00a035cd9b60c946dce55840cfbc5c
Omar SAIDI <omar.saidi@mediawan.com>
2022-01-14 09:21:49 +0100

    work queue

--- work-queues/NewTask/NewTask.cs
+++ work-queues/NewTask/NewTask.cs
@@ -1,31 +1,36 @@
-﻿using System;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
+using System;
 using System.Text;
 
-class Send
+class NewTask
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
         var factory = new ConnectionFactory { HostName = "localhost" };
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
-            channel.QueueDeclare(queue: "hello",
+            channel.QueueDeclare(queue: "task_queue",
                                  durable: false,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
-
-            string message = "Hello World!";
+            var message = GetMessage(args);
             var body = Encoding.UTF8.GetBytes(message);
 
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
             channel.BasicPublish(exchange: "",
-                                 routingKey: "hello",
-                                 basicProperties: null,
+                                 routingKey: "task_queue",
+                                 basicProperties: properties,
                                  body: body);
-            Console.WriteLine(" [x] Sent {0}", message);
         }
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
+    }
+    private static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
     }
 }
